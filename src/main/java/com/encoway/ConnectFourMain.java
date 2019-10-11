4a5bc52c224b99d69418b2534bb1487ce2b6689d
Alex <alexander.kaiser@greselius.net>
2019-10-11 09:43:21 +0200

    F V7

--- src/main/java/com/encoway/ConnectFourMain.java
+++ src/main/java/com/encoway/ConnectFourMain.java
@@ -12,8 +12,6 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 public class ConnectFourMain {
 
     public static Grid grid = new Grid();
@@ -25,29 +23,43 @@ public class ConnectFourMain {
         networking.instruct();
 
         grid.print();
-        while (!won && Listener.gotPacket && Listener.packet.getPacketType() == PacketType.TAKE_CONTROL) {
-            System.out.println("Gib eine Spalte ein: ");
-            System.out.println("Packet Type: " + Listener.packet.getPacketType().toString());
-            System.out.println("Received Info: " + Listener.gotPacket);
-            int row = 0;
-            boolean valid = false;
-
-            do {
-                try {
-                    row = new Scanner(System.in).nextInt();
-                    valid = true;
-                } catch (InputMismatchException e) {
-                    System.out.println("Nutze bitte nur Nummern!");
-                    valid = false;
+        while (!won) {
+            while(Listener.gotPacket) {
+                if(Listener.packet.getPacketType() == PacketType.TAKE_CONTROL) {
+                    int row = insertCoinViaControl();
+                    Listener.gotPacket = false;
+                    networking.send(Listener.lastOpponentIp, 80, new Packet(PacketType.PLACE_CHIP, row));
+                    networking.send(Listener.lastOpponentIp, 80, new Packet(PacketType.TAKE_CONTROL, 0));
+                    grid.print();
+                }else if(Listener.packet.getPacketType() == PacketType.PLACE_CHIP){
+                    Packet packet = Listener.packet;
+                    int row = packet.getData();
+                    grid.insertCoin(row, 0, currentPlayer);
+                    grid.print();
                 }
-            } while (!valid);
-
-            grid.insertCoin(row, 0, currentPlayer);
-            Listener.gotPacket = false;
-            networking.send(Listener.lastOpponentIp, 80, new Packet(PacketType.PLACE_CHIP, row));
-            networking.send(Listener.lastOpponentIp, 80, new Packet(PacketType.TAKE_CONTROL, 0));
-            grid.print();
+            }
         }
         System.out.println("You won!");
+    }
+
+    private static int insertCoinViaControl(){
+        System.out.println("Gib eine Spalte ein: ");
+        System.out.println("Packet Type: " + Listener.packet.getPacketType().toString());
+        System.out.println("Received Info: " + Listener.gotPacket);
+        int row = 0;
+        boolean valid = false;
+
+        do {
+            try {
+                row = new Scanner(System.in).nextInt();
+                valid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Nutze bitte nur Nummern!");
+                valid = false;
+            }
+        } while (!valid);
+
+        grid.insertCoin(row, 0, currentPlayer);
+        return row;
     }
 }
